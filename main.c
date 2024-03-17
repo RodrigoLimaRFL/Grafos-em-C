6f8ed4e57fc03a11554e33dddca4012af8437098
Maicon Chaves <118189820+MaiconChavesMarques@users.noreply.github.com>
2024-03-17 16:37:58 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "graph.h"
+#include <stdbool.h>
 
 int main(){
-    GRAPH *grafo1 = MyGraph(4);
-    
-    add_vertex(grafo1, 1);
-    add_vertex(grafo1, 5);
-    add_vertex(grafo1, 6);
-    add_vertex(grafo1, 0);
 
-    add_edge(grafo1, 1, 5, 1);
-    add_edge(grafo1, 1, 6, 2);
-    add_edge(grafo1, 1, 0, 3);
-    add_edge(grafo1, 5, 6, 4);
-    add_edge(grafo1, 5, 0, 5);
-    add_edge(grafo1, 0, 0, 6);
-    remove_edge(grafo1, 0, 0);
+    printf("Digite o numero de vertices que deseja que o grafo tenha:\n");
+    int n;
+    scanf("%d", &n);
 
-    menor_peso_graph(grafo1);
+    GRAPH *grafo1 = MyGraph (n); // a)
+    print_info(grafo1);
+    char comando[30];
+    bool teste = true;
 
-    print_matrix(grafo1);
-    int** mat = adjacency_matrix(grafo1);
-
-    for(int i = 0; i < number_of_vertexs(grafo1); i++)
-    {
-        for(int j = 0; j < number_of_vertexs(grafo1); j++)
-        {
-            printf("%d ", mat[i][j]);
+    while(teste){
+        scanf(" %s", comando);
+        if((strcmp(comando, "ADC_ARESTA"))==0){             // h)  
+            int vertice01, vertice02, peso;
+            scanf("%d %d %d", &vertice01, &vertice02, &peso);
+            add_edge(grafo1, vertice01, vertice02, peso);
+            print_info(grafo1);
         }
-        printf("\n");
+        if((strcmp(comando, "ADC_VERTICE"))==0){
+            int vertice01;
+            scanf("%d", &vertice01);
+            add_vertex(grafo1, vertice01);
+            print_info(grafo1);
+        }
+        if((strcmp(comando, "EXISTE_ARESTA"))==0){    //b)
+            int vertice01, vertice02;
+            scanf("%d %d", &vertice01, &vertice02);
+            exist_edge(grafo1, vertice01, vertice02);
+        }
+        if((strcmp(comando, "ADJACENTES_VERTICE"))==0){  //c)
+            int vertice01;
+            scanf("%d", &vertice01);
+            get_adj_vertex(grafo1, vertice01);
+        }
+        if((strcmp(comando, "REMOVE_ARESTA"))==0){ //d)
+            int vertice01, vertice02;
+            scanf("%d %d", &vertice01, &vertice02);
+            remove_edge(grafo1, vertice01, vertice02);
+            print_info(grafo1);
+        }
+        if((strcmp(comando, "IMPRIMIR_GRAFO"))==0){ //e)
+            print_info(grafo1);
+        }
+        if((strcmp(comando, "NUMERO_VERTICES"))==0){ //f)
+            number_of_vertexs(grafo1);
+        }
+        if((strcmp(comando, "REMOVER_GRAFO"))==0){  //g)
+            remove_graph(&grafo1);
+            teste = false;
+        }
+        if((strcmp(comando, "REMOVER_MENOR_PESO"))==0){  //i)
+            print_info(grafo1);
+            menor_peso_graph(grafo1);
+            print_info(grafo1);
+        }
+        if((strcmp(comando, "MATRIZ_ADJACENCIA"))==0){  //j)
+            int** mat = adjacency_matrix(grafo1);
+            for(int i = 0; i < number_of_vertexs(grafo1); i++){
+                for(int j = 0; j < number_of_vertexs(grafo1); j++){
+                    printf("%d ", mat[i][j]);
+                }       
+            printf("\n");
+            }
+            for(int i = 0; i < number_of_vertexs(grafo1); i++){
+                free(mat[i]);
+            }
+            free(mat);
+        }
+        if((strcmp(comando, "SAIR"))==0){
+            remove_graph(&grafo1);
+            teste = false;
+        }
     }
-
-    for(int i = 0; i < number_of_vertexs(grafo1); i++)
-    {
-        free(mat[i]);
-    }
-    free(mat);
-
-    print_info(grafo1); 
-
-    remove_graph(&grafo1);
 
     return 0;
 }
